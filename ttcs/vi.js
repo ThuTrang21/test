55a3333a42ddc7dfbc6d14610a4ae780c18ae6d3
ThuTrang21 <133649471+ThuTrang21@users.noreply.github.com>
2023-07-08 17:01:56 +0700

    Delete ttcs directory

--- ttcs/vi.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("tool-list").style.display="none"
-function showcart(){
-    var x=document.getElementById("tool-list");
-    if(x.style.display=="block"){
-        x.style.display="none";
-    }
-    else x.style.display="block";
-}
